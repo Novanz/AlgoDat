3d00ceb7e1cfbe474e01436305f7734efc11d4f4
Novan@820 <55051643+Novanz@users.noreply.github.com>
2024-03-19 21:12:59 +0100

    skel:   Bruch.java

---
+++ B2/Bruch.java
@@ -0,0 +1,28 @@
+public class Bruch{
+    private int num; // Zähler
+    private int denom; // Nenner
+
+    public Bruch(int num, int denom) {
+        this.num = num;
+        this.denom = denom;
+    }
+
+    /* getter */
+    public Bruch get() { // FIX:
+        return this.Bruch;
+    }
+
+    public int getN() {
+        return num;
+    }
+
+    public int getD() {
+        return num;
+    }
+
+    /* printer */
+    // TODO:
+
+    /* Arithmetik */
+    // TODO: add(),sub(),div(),mult()
+}
