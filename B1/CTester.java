0f5b68c2b7347869279d4db288ba5b30e0c96c76
Novan@820 <55051643+Novanz@users.noreply.github.com>
2024-03-19 18:20:44 +0100

    Tester for restore() method in Counter.java

---
+++ B1/CTester.java
@@ -0,0 +1,21 @@
+public class CTester {
+    public static void main(String[] args) {
+        Counter a = new Counter(); // Erzeugen eines Zählers a
+//        Counter b = new Counter(); // Erzeugen eines Zählers b
+     
+        for(int i = 0; i < 10; ++i) {
+            a.increment(); // Hochzählen von a
+        }
+//        a.save();// Aktueler Wert wird gesichert(hier 10)
+         
+        for(int i = 5; i > 0; i--) {
+            a.decrement(); // Runterzählen von a
+        }
+     
+        System.out.println("a=" + a.get() + " | soll: 5" ); // Liefert: a=5
+        a.restore(); // Soll hier Fehler werfen und beenden
+        System.out.println("a (nach restore)=" + a.get() + " | soll: 10"); // Liefert: a=10
+        a.reset();
+        System.out.println("a (nach reset)=" + a.get()); // Liefert?
+    }
+}
