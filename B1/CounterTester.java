eeef66dfad0e0c3aaec0bc05ebb00fd3fa2ada28
Novan@820 <55051643+Novanz@users.noreply.github.com>
2024-03-19 08:58:34 +0100

    modified:   CounterTester.java

--- B1/CounterTester.java
+++ B1/CounterTester.java
@@ -3,8 +3,10 @@ public class CounterTester {
         Counter a = new Counter(); // Erzeugen eines Zählers a
         Counter b = new Counter(); // Erzeugen eines Zählers b
      
-        a.increment(); // Hochzählen von a
-        a.increment(); // Nochmaliges Hochzählen von a
+        for(int i = 0; i < 10; ++i) {
+            a.increment(); // Hochzählen von a
+        }
+//            a.increment(); // Nochmaliges Hochzählen von a
      
         System.out.println("a=" + a.get()); // Liefert: a=2
         System.out.println("b=" + b.get()); // Liefert: b=0
